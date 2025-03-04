07e81b64b88f2766ecad3951ffc71662e3d6019d
Elisa Chiappini <elisabichiappini@gmail.com>
2025-03-04 21:41:23 +0100

    feat: definire le funzioni per l'autenticazione

---
+++ controllers/auth.js
@@ -0,0 +1,14 @@
+// funzione per generare i token
+const generateToken = (payload, expiresIn = "10s") => {
+
+}
+
+//middleware per verificare il token
+const authenticateWithJWT = (req,res,next) => {
+
+}
+
+//controller per generare il token 
+const login = (req, res) => {
+
+}
